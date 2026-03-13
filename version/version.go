e11f0fe0903e89e52f2d5ebf7d6cdf276a190eb4
Ryan Johnson <ryan.johnson@broadcom.com>
2026-03-13 08:37:36 -0700

    chore(dev): update development cycle version (#678)
    Update the plugin for the 2.1.2 development cycle and pre-release builds.
    
    Signed-off-by: Ryan Johnson <ryan.johnson@broadcom.com>

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ package version
 import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
-	Version           = "2.2.0"
+	Version           = "2.1.2"
 	VersionPrerelease = "dev"
 	VersionMetadata   = ""
 	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
